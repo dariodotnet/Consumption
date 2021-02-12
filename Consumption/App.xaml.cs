edec74cc75b6b252421e581eb95dcc3863a7779b
Dario Alonso <dariodotnet@gmail.com>
2021-02-12 20:05:33 +0000

    Avoid creating fake data

--- Consumption/App.xaml.cs
+++ Consumption/App.xaml.cs
@@ -3,9 +3,7 @@
 namespace Consumption
 {
     using SQLite;
-    using System;
     using System.IO;
-    using System.Linq;
     using Xamarin.Essentials;
 
     public partial class App : Application
@@ -26,28 +24,6 @@ namespace Consumption
             DataBase = new SQLiteConnection(_databasePath);
 
             DataBase.CreateTable<Item>();
-
-            var dummyData = App.DataBase.Table<Item>().ToList();
-
-            if (dummyData == null || !dummyData.Any())
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    var tag = "Electricity";
-                    if (i > 49)
-                    {
-                        tag = "Gas";
-                    }
-
-                    var item = new Item
-                    {
-                        DateTime = DateTime.UtcNow.AddDays(-100).AddDays(i),
-                        Description = $"Dummy data {i}",
-                        Tag = tag
-                    };
-                    DataBase.Insert(item);
-                }
-            }
         }
 
         protected override void OnSleep()
